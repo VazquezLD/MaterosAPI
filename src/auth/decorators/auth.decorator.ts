7fa3177e366bf5702260e20847afd133e5a43c34
Luciano <lucianovazquez.lv15@gmail.com>
2026-04-06 17:49:54 -0300

    fix(productos): Error en el modulo de productos con el decorador Auth arreglado

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -5,6 +5,6 @@ import { UserRoleGuard } from '../guards/user-role.guard';
 export function Auth(...roles: string[]) {
   return applyDecorators(
     SetMetadata('roles', roles),
-    UseGuards(AuthGuard(), UserRoleGuard),
+    UseGuards(AuthGuard('jwt'), UserRoleGuard),
   );
 }
--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -3,12 +3,14 @@ import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Producto, ProductoSchema } from './entities/producto.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductoController],
   providers: [ProductoService],
   imports: [
-    MongooseModule.forFeature([{ name: Producto.name, schema: ProductoSchema }])
+    MongooseModule.forFeature([{ name: Producto.name, schema: ProductoSchema }]),
+    AuthModule
   ],
   exports: [MongooseModule]
 })
